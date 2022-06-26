bbc3e1bfda111fbc22fa751b0e52e367cc5852d0
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-06-26 09:44:30 -0300

    Estudo variável/constante

---
+++ variáveis/variavel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+// VARIÁVEIS:
+    int x;
+        x = 5;
+    int y;
+        y = 2;
+    printf("%d, %d\n", x, y);
+    printf("%d x %d = %d\n", 5, 3, 5*3);
+// CONSTANTE:
+    int const constante = 1;
+    printf("%d\n", constante);
+// CONSTANTE NÃO PODE SER TROCADA, MAS VARIÁVEL PODE
+}
