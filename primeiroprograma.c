21e6a8cc4c28971b5180d3f253008c0f8a374956
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-06-26 09:03:59 -0300

    testando comentários

--- primeiroprograma.c
+++ primeiroprograma.c
@@ -1,6 +1,10 @@
+// Comando abaixo serve para baixar bilbiotecas
 #include <stdio.h>
+// Todos programa em C deve-se iniciar com "main()"
 
-main():
+int main()
 {
 	printf("Ola Neps Academy!\n");
+	printf("Testando Quebra de Linha/n");
+	return 2;
 }
