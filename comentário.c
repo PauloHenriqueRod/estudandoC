648d3c020129e06f4f5ea5715cc56b2619a7e123
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-06-27 22:11:04 -0300

    Create comentário.c

---
+++ comentário.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int   a,b; //Declaro minhas variáveis a e b.
+
+    scanf("%d%d", &a, &b); //Leio dois valores
+
+    printf("soma = %d\n", a+b);  //Imprimo a soma de a+b
+}
