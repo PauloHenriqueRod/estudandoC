2ea030acdee1db45aa5c6313e5e31635f6bfb15e
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-07-06 20:28:05 -0300

    estudo

---
+++ operadores de lógica/if_else_if.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float media;
+    printf("MEDIA: ");
+    scanf("%f", &media);
+    if (media >= 7){
+        printf("APROVADO\n");
+    }else if (media < 4){
+        printf("REPROVADO\n");
+    }else{
+        printf("EXAME FINAL\n");
+    }
+}
