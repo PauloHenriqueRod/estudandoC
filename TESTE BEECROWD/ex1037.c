dcc1b964cd162a1dd000b7665c84f24c90830f22
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-07-16 11:10:48 -0300

    Create ex1037.c

---
+++ TESTE BEECROWD/ex1037.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    float n;
+    scanf("%f", &n);
+    if (n >= 0 && n<=100){
+        if (n <= 25)
+            printf("Intervalo [0,25]");
+        if (n > 25 && n <= 50)
+            printf("Intervalo (25,50]");
+        if (n > 50 && n <= 75)
+            printf("Intervalo (50,75]");
+        if (n > 75 && n <= 100)
+            printf("Intervalo (75,100]");
+    }else
+    printf("Fora de intervalo");
+    return 0;
+}
