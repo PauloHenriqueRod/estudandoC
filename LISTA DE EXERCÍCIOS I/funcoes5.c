eaae770039896d0fea6c846511890f7b28db8cea
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-09-17 16:43:24 -0300

    ult ex função

---
+++ LISTA DE EXERCÍCIOS I/funcoes5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int LeNaoNegativo(){
+    int num;
+    while(1){
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &num);
+    if (num>=0)
+        return num;
+    }
+}
+
+int SomaDivisores(int x){
+    int soma=0;
+    for (int i=1; i<=(x/2); i++){
+        if (x%i==0)
+            soma += i;
+    }
+    return soma;
+}
+
+int SaoAmigos(int n, int m){
+    if ((SomaDivisores(n)==m)&&(SomaDivisores(m)==n))
+        return 1;
+    else
+        return 0;
+}
+
+
+int main(){
+    int x, y, t;
+    x = LeNaoNegativo(x);
+    y = LeNaoNegativo(y);
+    t = SaoAmigos(x, y);
+    if (t==1)
+        printf("Sao amigos");
+    else
+        printf("Nao sao amigos");
+    return 0;
+}
